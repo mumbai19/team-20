a1156dca3ecddc6d9c9b4f2cbef812fa38433214
karu007 <hemdevkaran07@gmail.com>
2019-07-20 20:46:35 +0530

    added

---
+++ trishul/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('UI.index');
+});
+
+Route::get('/product', function () {
+    return view('UI.product');
+});
+
+Route::get('/category', function () {
+    return view('UI.category');
+});
+
+Route::get('/cart', function () {
+    return view('UI.cart');
+});
+
+Route::get('/donate', function () {
+    return view('UI.donate');
+});
+
+Route::get('/contact', function () {
+    return view('UI.contact');
+});
