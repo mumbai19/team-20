45ecefc28fe5afef0b589ee7e6c797e0333fc1f7
SaurabhS <saurabh.s1999@gmail.com>
2019-07-20 23:28:30 +0530

    Migrations

---
+++ trishul/app/Http/Controllers/OrdersController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Orders;
+use Illuminate\Http\Request;
+
+class OrdersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Orders  $orders
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Orders $orders)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Orders  $orders
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Orders $orders)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Orders  $orders
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Orders $orders)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Orders  $orders
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Orders $orders)
+    {
+        //
+    }
+}
